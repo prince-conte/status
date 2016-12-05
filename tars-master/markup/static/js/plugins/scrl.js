cb820ee415f33a9bf6bdaac3188e519e1cac1de0
prince-conte <prince-conte@outlook.com>
2016-12-05 22:32:52 +0300

    +top

--- tars-master/markup/static/js/plugins/scrl.js
+++ tars-master/markup/static/js/plugins/scrl.js
@@ -3,6 +3,8 @@
  var burgerClick = 0;   
      
 $("a").click(function () {
+$(".topmenu").removeClass('mobile');
+burgerClick = 0; 
 var elementClick = $(this).attr("href")
 var destination = $(elementClick).offset().top;
 jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
@@ -15,20 +17,45 @@ $("#burger").click(function () {
 if (burgerClick == 0)  
     
 {
-$(".navlink").css('display' , 'table');
+$(".topmenu").addClass('mobile');
 burgerClick = 1;
 }
     
 else 
 {
     
-$(".navlink").css('display' , 'none');
+$(".topmenu").removeClass('mobile');
 burgerClick = 0;   
     
 }
     
 });
-        
+     
+  
+     
+$(window).scroll(function() {
+     
+var scrollBottom =  $(document).height();
+      
+      
+if (($(this).scrollTop() >= 700)) {
+         
+$('#top').fadeIn();
+
+
+         
+} else {
+    
+$('#top').fadeOut();
+         
+} 
+  
+  
+
+});   
+
+       
+
      
 });
     
