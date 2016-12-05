f8d936c1adb87a1b8c6715e6f21fcd5459497786
francesco <prince-conte@outlook.com>
2016-12-05 17:51:45 +0300

    vou

--- tars-master/markup/static/js/plugins/scrl.js
+++ tars-master/markup/static/js/plugins/scrl.js
@@ -1,13 +1,34 @@
  $(document).ready(function() {
 
-
+ var burgerClick = 0;   
+     
 $("a").click(function () {
 var elementClick = $(this).attr("href")
 var destination = $(elementClick).offset().top;
 jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
 return false;
 });
+
      
+$("#burger").click(function () {  
+    
+if (burgerClick == 0)  
+    
+{
+$(".navlink").css('display' , 'table');
+burgerClick = 1;
+}
+    
+else 
+{
+    
+$(".navlink").css('display' , 'none');
+burgerClick = 0;   
+    
+}
+    
+});
+        
      
 });
     
